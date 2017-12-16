dcb90fc724fe521c117d6db8c1916d580e2d0c9d
Alex Ramirez <alexandergramirez@gmail.com>
2017-12-16 00:01:02 -0600

    initial code commit

---
+++ src/Kasi.Tools.Harvester.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasi.Tools.Harvester.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/main.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace Kasi.Harvester {
+    public static void Main() {
+        return 0;
+    }
+}
