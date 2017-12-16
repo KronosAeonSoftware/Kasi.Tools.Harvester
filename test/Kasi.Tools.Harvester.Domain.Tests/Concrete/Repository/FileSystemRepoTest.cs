509c52e74707237b29438d55d4042ef19c3e4db5
Alex Ramirez <alexandergramirez@gmail.com>
2017-12-16 11:14:46 -0600

    Aadding unit test project for the Kasi.Tools.Harvester.Domain project

---
+++ test/Kasi.Tools.Harvester.Domain.Tests/Concrete/Repository/FileSystemRepoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kasi.Tools.Harvester.Domain.Tests.Concrete.Repository
+{
+    [TestClass]
+    public class FileSystemRepoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ test/Kasi.Tools.Harvester.Domain.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kasi.Tools.Harvester.Domain.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kasi.Tools.Harvester.Domain.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f807c3bb-c528-4670-bcb6-0a763d3533c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
