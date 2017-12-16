b2a1be894a93281af437c9ad55c336a0f6ed1617
Alex Ramirez <alexandergramirez@gmail.com>
2017-12-16 01:18:12 -0600

    Adding basic functionality for a file system entity class

---
+++ src/Kasi.Tools.Harvester.Domain/Concrete/Entity/FileSystemRecord.cs
@@ -0,0 +1,49 @@
+﻿using Kasi.Tools.Harvester.Domain.Abstract.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kasi.Tools.Harvester.Domain.Concrete.Entity
+{
+    public class FileSystemRecord : IFileSystemRecord
+    {
+        #region " Properties "
+
+        public Guid Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string URI => throw new NotImplementedException();
+
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Extension { get; set; }
+
+        public string ContentsHash => throw new NotImplementedException();
+        public string FilelPathHash => throw new NotImplementedException();
+
+        public string Contents => throw new NotImplementedException();
+
+        public string FileName { get { return String.Format("{0}.{1}", Name, Extension.TrimStart('.')); } }
+        public string FilePath { get { return System.IO.Path.Combine(Path, FileName); } }
+
+        #endregion
+
+
+        #region " Validation "
+
+        public bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+        public void Validate(bool stopOnFirstError)
+        {
+            throw new NotImplementedException();
+        }
+        public string[] ValidationErrors(bool stopOnFirstError)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
